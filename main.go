7274d0dc1e066495b0b9c5064e8cb3206c8d4aef
Steve Morrissey <uberamd@gmail.com>
2017-05-19 09:13:43 -0500

    updating regex matching

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"log"
+	"regexp"
 )
 
 var githash string
@@ -10,12 +11,25 @@ var builddate string
 
 func redirect(w http.ResponseWriter, r *http.Request) {
 	var urlScheme string = "http"
+	var host string = r.Host
+
+	reg, err := regexp.Compile(`\:\d+$`)
+
+	if err != nil {
+		log.Printf("Error in regex parsing: %v", err)
+	}
 
 	if(r.Header.Get("X-Forwarded-Proto") == "https") {
 		urlScheme = "https"
 	}
 
-	var fullUrl string = urlScheme + "://www." + r.Host + r.URL.Path
+	if(r.Header.Get("X-Forwarded-For") != "") {
+		host = r.Header.Get("X-Forwarded-For")
+	}
+
+	safe := reg.ReplaceAllString(host, "")
+
+	var fullUrl string = urlScheme + "://www." + safe + r.URL.Path
 
 	log.Printf("Redirecting to: %v", fullUrl)
 	http.Redirect(w, r, fullUrl, 301)
